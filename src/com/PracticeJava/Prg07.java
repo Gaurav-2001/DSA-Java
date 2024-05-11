af13231100f1dc230738903347afc0d64988f4cf
Gaurav-2001 <gauravpagare2001@gmail.com>
2024-05-11 23:56:08 +0530

    Taking inputs from command line

---
+++ src/com/PracticeJava/Prg07.java
@@ -0,0 +1,10 @@
+package com.PracticeJava;
+
+public class Prg07 {
+    // taking input from command line
+    public static void main(String[] args) {
+        for(String i: args){
+            System.out.println(i);
+        }
+    }
+}
