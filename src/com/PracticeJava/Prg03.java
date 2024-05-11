8fa0674a02e23597af47f66b4ad1a84119e766b8
Gaurav-2001 <gauravpagare2001@gmail.com>
2024-05-11 20:13:56 +0530

    Added the if else example

---
+++ src/com/PracticeJava/Prg03.java
@@ -0,0 +1,19 @@
+package com.PracticeJava;
+
+public class Prg03 {
+    public static void main(String[] args) {
+        // if else block
+        boolean x=5>6;
+        if (x){
+            System.out.println("this is true statement");
+        }
+        else if (x){
+            System.out.println("this is if else statement");
+        }
+        else {
+            System.out.println("this is a else statement");
+        }
+
+
+    }
+}
