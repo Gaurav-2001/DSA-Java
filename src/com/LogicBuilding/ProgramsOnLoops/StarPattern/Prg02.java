eb9fbfe5a07fbbbe1487d795e37ab3a6dc3be210
Gaurav-2001 <gauravpagare2001@gmail.com>
2024-05-20 13:48:42 +0530

    Star problem

--- src/com/LogicBuilding/ProgramsOnLoops/StarPattern/Prg02.java
+++ src/com/LogicBuilding/ProgramsOnLoops/StarPattern/Prg02.java
@@ -1,10 +1,13 @@
 package com.LogicBuilding.ProgramsOnLoops.StarPattern;
 
-public class Prg01 {
+public class Prg02 {
     public static void main(String[] args) {
         for (int i = 1; i <=5 ; i++) {
-            for (int j = 1; j <= i ; j++) {
-                System.out.print(" * ");
+            for (int j = 1; j <= 5 ; j++) {
+                if(j>=6-i)
+                    System.out.print(" * ");
+                else
+                    System.out.print("   ");
             }
             System.out.print("\n");
         }
---
+++ src/com/LogicBuilding/ProgramsOnLoops/StarPattern/Prg07.java
@@ -0,0 +1,16 @@
+package com.LogicBuilding.ProgramsOnLoops.StarPattern;
+
+public class Prg07 {
+    public static void main(String[] args) {
+        int n =5;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=(2*n-1) ; j++) {
+                if(j<=6-i || j>=4+i)
+                    System.out.print(" * ");
+                else
+                    System.out.print("   ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
