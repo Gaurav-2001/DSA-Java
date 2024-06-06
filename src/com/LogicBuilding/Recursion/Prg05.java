0c953ebfa01dfa00cb4413e21f193261053beabc
Gaurav-2001 <gauravpagare2001@gmail.com>
2024-06-06 09:54:11 +0530

    recursion practice

---
+++ src/com/LogicBuilding/Recursion/Prg05.java
@@ -0,0 +1,18 @@
+package com.LogicBuilding.Recursion;
+
+public class Prg05 {
+    public static void main(String[] args) {
+//        System.out.print(-135/10);
+        int n = 1342;
+        System.out.printf("sum of digits in %d is %d",n,digit_sum(n));
+    }
+    private static int digit_sum(int n){
+        if (n<0){
+            n=-n;
+        }
+        if (n<10){
+            return n;
+        }
+        return n%10 + digit_sum(n/10);
+    }
+}
